7cc0e0260e5e9ef46976591eaec4815ab7b9c5fd
SirSaleh <animatorsaleh@gmail.com>
2024-05-14 01:56:07 +0330

    Add Product model and migration files

--- bitpin_task/market/models.py
+++ bitpin_task/market/models.py
@@ -1,3 +1,10 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(verbose_name=_("Title"), max_length=128)
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
