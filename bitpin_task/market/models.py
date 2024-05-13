839fb4d5e6b73546f94916164c40badab5176462
SirSaleh <animatorsaleh@gmail.com>
2024-05-14 02:28:52 +0330

    Add Product rating model

--- bitpin_task/market/models.py
+++ bitpin_task/market/models.py
@@ -1,4 +1,5 @@
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -8,3 +9,16 @@ class Product(models.Model):
     class Meta:
         verbose_name = _("Product")
         verbose_name_plural = _("Products")
+
+
+class ProductRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("User"))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_("Product"))
+    rating = models.SmallIntegerField(verbose_name=_("Rating"))
+
+    class Meta:
+        verbose_name = _("Product Rating")
+        verbose_name_plural = _("Product Ratings")
+
+        unique_together = ("user", "product")
+
