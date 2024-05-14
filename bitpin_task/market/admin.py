a91d053bcb7497fc4f7d2bf2b00a6e5b536118a2
SirSaleh <animatorsaleh@gmail.com>
2024-05-14 11:26:47 +0330

    Add api app initially

--- bitpin_task/market/admin.py
+++ bitpin_task/market/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, ProductRating
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductRatingAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductRating, ProductRatingAdmin)
