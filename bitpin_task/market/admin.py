4f894336fa8f7059f254aee18e62f6984206026b
SirSaleh <animatorsaleh@gmail.com>
2024-05-17 02:37:45 +0330

    Add list display to admin pages

--- bitpin_task/market/admin.py
+++ bitpin_task/market/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Product, ProductRating
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title']
 
 
 class ProductRatingAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'rating']
 
 
 admin.site.register(Product, ProductAdmin)
