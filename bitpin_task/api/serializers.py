137503593291989758093dc513e2ec5d323acb9e
SirSaleh <animatorsaleh@gmail.com>
2024-05-17 05:51:18 +0330

    Implement hourly average approach for overal rating

--- bitpin_task/api/serializers.py
+++ bitpin_task/api/serializers.py
@@ -1,13 +1,24 @@
 from django.contrib.auth.models import User
+from django.db.models import Avg
+from django.db.models.functions import TruncHour
+from market.models import ProductRating
 from rest_framework import serializers
 from market.models import Product, ProductRating
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    overal_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = ['id', 'title']
+        fields = ['id', 'title', "overal_rating"]
+    
+    def get_overal_rating(self, obj):
+        hourly_rating = ProductRating.objects.annotate(hour=TruncHour('timestamp'))
+        hourly_average_ratings = hourly_rating.values('hour').annotate(hourly_avg=Avg('rating'))
+        overal_rating = hourly_average_ratings.aggregate(overall_avg=Avg('hourly_avg'))
+
+        return round(overal_rating['overall_avg'], 2)
 
 
 class UserSerializer(serializers.ModelSerializer):
